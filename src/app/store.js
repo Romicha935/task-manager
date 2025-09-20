c423b84fd5619ff8da226b3a1f51b45a7ad63f6e
romicha-parvin <romichaparvin35@gmail.com>
2025-09-20 18:42:07 +0600

    add task added

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+//import { tasksReducer } from "../features/tasks/tasksSlice";
 
 export const store = configureStore({
     reducer : {
-        tasks : taskReducer,
+        // tasks : tasksReducer,
     }
 })
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addtasks } from '../features/tasks/tasksSlice'
+
+const AddTask = () => {
+    const [text,setText] = useState('')
+    const dispatch = useDispatch()
+
+    const handleAdd = ()=> {
+       if(text.trim()){
+        dispatch(addtasks(text))
+        setText("")
+       }
+    }
+  return (
+    <div className='flex gap-2 mb-4'>
+       <input value={text}
+         onChange={(e)=> setText(e.target.value)}
+       type="text" placeholder='Enter a task'  className='border rounded px-3  py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-400' />
+       <button onClick={handleAdd} className='bg-blue-400 text-white rounded px-4 py-2 hover:bg-blue-700'>Add</button>
+    </div>
+  )
+}
+
+export default AddTask
